12552e0f17192cb7b21f5e6e8451ea9672b31ffc
Sushma <85895355+Sushma-TSC@users.noreply.github.com>
2022-01-20 10:21:02 -0500

    injectVideo to DOM

--- tests/cam.spec.mjs
+++ tests/cam.spec.mjs
@@ -1,13 +1,19 @@
 import { test } from '@playwright/test';
-
+import { injectVideo } from '../utilities/helper.mjs'
 test.describe.serial('Camera Test', () => {
     let page;
 
     test.beforeAll(async({ browser }) => {
-
+        async function cursor(p) {
+            await p.addInitScript(function() {
+                navigator.mediaDevices.enumerateDevices = () => Promise.resolve([{ deviceId: 'blah', groupId: 'foo', kind: 'videoinput', label: '' }]);
+                window.addEventListener('DOMContentLoaded', injectVideo);
+            });
+            return p;
+        }
         const context = await browser.newContext();
-        await context.grantPermissions(['camera']);
-        page = await context.newPage();
+        // await context.grantPermissions(['camera']);
+        page = await context.newPage().then(cursor);
         await page.goto("https://www.onlinemictest.com/webcam-test/");
 
     });
